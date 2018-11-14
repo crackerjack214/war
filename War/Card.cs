e3512dd04ab00b810e57288c1ced1bb9bb4c5721
Jason <jason.m.miraglia@gmail.com>
2018-11-14 11:52:08 -0500

    Cleanup unneeded dependencies

--- War/Card.cs
+++ War/Card.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace War
+﻿namespace War
 {
+    // Card object contains value, suit, print value conversion, and card comparison
     public class Card
     {
-        public Suit suit { get; set; }      
-        public int rawValue { get; set; }   // Raw value of the card 2-14
+        public Suit suit;
+        // Raw value of the card 2-14
+        public int rawValue;
 
-        public Card(Suit cardSuit, int value) {
+        // Initialize a new card containing a suit and a raw value
+        public Card(Suit cardSuit, int value) 
+        {
             suit = cardSuit;
             rawValue = value;
         }
 
         // Return the value that would be printed on the card 2-10, J, Q, K, A
         public string GetPrintValue () {
-            string printValue = rawValue.ToString();
+            string printValue;
 
             // If card value is greater than 10 map it's face value
             switch (rawValue)
@@ -32,27 +33,31 @@ namespace War
                 case 14:
                     printValue = "A";
                     break;
+                default:
+                    printValue = rawValue.ToString();
+                    break;
             }
 
             return printValue;
-     
         }
 
         // Check if this card is higher than a comparison card
         public int IsHigherThan(Card otherCard)
         {
+            // -1 indicates a tie
             int isWinner = -1;
 
             if (otherCard.rawValue > rawValue)
             {
+                // other card is winner
                 isWinner = 1;
             } else if (otherCard.rawValue < rawValue)
             {
+                // this card is winner
                 isWinner = 0;
             }
 
             return isWinner;
         }
-
     }
 }
